355d9c0b9268ed9188673e0b8a63349903689b82
Benny <s3781767@student.rmit.edu.au>
2023-09-06 06:29:08 +1000

    Added debouncing of 500ms

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import SearchBox from "./components/SearchBox/SearchBox";
@@ -6,32 +6,30 @@ import ResultsContainer from "./components/ResultsContainer/ResultsContainer";
 
 const name = require("@rstacruz/startup-name-generator");
 
-class App extends React.Component {
-  state = {
-    headerText: "Name It!",
-    headerExpanded: true,
-    suggestedNames: [],
+const App = () => {
+  const headerText = "Name It!";
+  const [headerExpanded, setHeaderExpanded] = useState(true);
+  const [suggestedNames, setSuggestedNames] = useState([]);
+
+  const handleInputChange = (input) => {
+    setHeaderExpanded(!input);
+    setSuggestedNames(input ? name(input) : []);
   };
 
-  handleInputChange = (input) => {
-    this.setState({
-      headerExpanded: !(input.length > 0),
-      suggestedNames: input ? name(input) : [],
-    });
-  };
+  return (
+    <div>
+      <Header headerExpanded={headerExpanded} headTitle={headerText} />
+      <SearchBox onInputChange={handleInputChange} />
+      <ResultsContainer suggested={suggestedNames} />
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <Header
-          headerExpanded={this.state.headerExpanded}
-          headTitle={this.state.headerText}
-        />
-        <SearchBox onInputChange={this.handleInputChange} />
-        <ResultsContainer suggested={this.state.suggestedNames} />
-      </div>
-    );
-  }
-}
+// class App extends React.Component {
+//   state = {
+//     headerText: "Name It!",
+//     headerExpanded: true,
+//     suggestedNames: [],
+//   };
 
 export default App;
--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,10 +1,25 @@
 import styles from "./SearchBox.module.css";
+import React, { useState, useMemo } from "react";
+import { debounce } from "lodash";
 
 function SearchBox({ onInputChange }) {
+  const [value, setValue] = useState("test");
+  console.log(value);
+
+  function changeHandler(input) {
+    setValue(input);
+    onInputChange(input);
+  }
+  // uses memo so that we cache the method and don't need to rerender it every single time it is called
+  const debounceHandler = useMemo(
+    () => debounce(changeHandler, 500),
+    [setValue]
+  );
   return (
     <div className={styles.searchContainer}>
       <input
-        onChange={(event) => onInputChange(event.target.value)}
+        // onChange={(event) => onInputChange(event.target.value)}
+        onChange={(event) => debounceHandler(event.target.value)}
         placeholder="Type keywords"
         className={styles.searchInput}
         spellCheck="false"
